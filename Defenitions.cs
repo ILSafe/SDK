94078663356feccd5667cec325345bd9da0776dd
Alikhan "Gamania" Khartumov <autocardag@gmail.com>
2022-10-25 20:03:06 +0300

    Initial commit

---
+++ Defenitions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ILSafe.SDK
+{
+    public enum SecurityType : byte
+    {
+        NONE,
+        SECURITY,
+        VIRTUALIZATION,
+        ALL
+    }
+
+    public enum SecurityOptions : byte
+    {
+        NONE,
+        ENCODESTRINGS,
+        FAKEILCODE,
+        CODEJUNK,
+        SUPRESSILDASM,
+        ANTIDEBUG,
+        ANTIVM,
+        INTCONFUSER,
+        LAZYIMPORT
+    }
+
+    public class SecurityTypeAttribute : Attribute
+    {
+        public SecurityType _type { get; set; }
+
+        public SecurityTypeAttribute(SecurityType type)
+        {
+            _type = type;
+        }
+    }
+
+    public class SecurityOptionsAttribute : Attribute
+    {
+        public List<SecurityOptions> _type { get; set; }
+
+        public SecurityOptionsAttribute(params SecurityOptions[] type)
+        {
+            _type = type.ToList();
+        }
+    }
+}
